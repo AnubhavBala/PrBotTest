77416046519e75309ea277258175bd68bf420117
Anubhav Balakrishnan <Anubhav.Balakrishnan@target.com>
2026-01-12 11:49:16 -0600

    woo

--- test/src/Main.java
+++ test/src/Main.java
@@ -8,9 +8,11 @@ void main() {
     //ewwww change this
     /// TODO: Refactor this loop to improve readability
 
+
     for (int i = 1; i <= 5; i++) {
         //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
         // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
         IO.println("i = " + i);
+        print sthings
     }
 }
